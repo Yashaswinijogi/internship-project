32b57ec737f2fd94c6daef9b94f94c81d4e8a8c9
Yashaswinijogi <yashaswinikiranjogi.com>
2023-06-26 01:29:06 -0700

    added navbar

--- includes/about.php
+++ includes/about.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electronics store</title>
+    <title>Electronics store | About</title>
     <?php include("./includes/header.php");?>
 </head>
 <body>
